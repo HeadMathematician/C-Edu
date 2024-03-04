a6d579026a2371f377fcb9a8033482827c7e414f
Teo Kupčinovac <teo@insa-software.com>
2024-03-04 20:39:23 +0100

    DbContext configured

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using BookWebShop.Data;
+using BookWebShop.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookWebShop.Controllers
+{
+
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> categoryList = _context.Categories.ToList();
+            return View(categoryList);
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,40 @@
+﻿using BookWebShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookWebShop.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Buyer> Buyers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category
+                {
+                    Id = 1,
+                    Name = "SciFi",
+                    DisplayOrder = 1,
+                },
+                new Category
+                {
+                    Id = 2,
+                    Name = "Novel",
+                    DisplayOrder = 2,
+                },
+                new Category
+                {
+                    Id = 3,
+                    Name = "Thriller",
+                    DisplayOrder = 3,
+                });
+        }
+    }
+}
+
