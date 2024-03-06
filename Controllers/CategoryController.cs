3b83838a8959036883a300c203add6f1d308cfc9
Teo Kupčinovac <teo@insa-software.com>
2024-03-06 22:53:15 +0100

    Implemented CRUD for Category grid

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -20,5 +20,60 @@ namespace BookWebShop.Controllers
             List<Category> categoryList = _context.Categories.ToList();
             return View(categoryList);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+
+            }
+            return View();
+        }
+
+        public ActionResult Delete(int categoryId)
+        {
+            var toDelete = _context.Categories.Where(x => x.Id == categoryId).FirstOrDefault();
+
+            if (toDelete == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categories.Remove(toDelete);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult Edit(int categoryId)
+        {
+            Category category = _context.Categories.Find(categoryId);
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            Category toUpdate = _context.Categories.Where(x => x.Id == category.Id).FirstOrDefault();
+
+            toUpdate.Name = category.Name;
+            toUpdate.DisplayOrder = category.DisplayOrder;
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
     }
 }
--- Models/Category.cs
+++ Models/Category.cs
@@ -1,12 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BookWebShop.Models
 {
     public class Category
     {
-        [Key]
         public int Id { get; set; }
+
+        [MaxLength(30)]
         public string Name { get; set; }
+
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
     }
 }
